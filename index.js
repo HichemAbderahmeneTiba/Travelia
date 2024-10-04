8c2a2ce1e0634d2fa255cf8981536e6299e99bba
Hichem Abderahmene Tiba <hichemabderahmenetiba@gmail.com>
2024-10-04 22:59:31 +0100

    finish basic bar

---
+++ index.js
@@ -0,0 +1,8 @@
+let list_item = document.querySelectorAll(".list-item");
+console.log(list_item);
+list_item.forEach(item => {
+  item.addEventListener("click", function () {
+    list_item.forEach(item => item.classList.remove("underlined"));
+    item.classList.add("underlined");
+  });
+});
