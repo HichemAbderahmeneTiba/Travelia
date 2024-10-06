9353e3dbe46d2ba75cada6f73080e527e804b669
Hichem Abderahmene Tiba <hichemabderahmenetiba@gmail.com>
2024-10-06 21:56:42 +0100

    finish the bar without animation

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 "use strict";
 
 let list_item = document.querySelectorAll(".list-item");
-console.log(list_item);
+let others = document.querySelector(".others");
+let menu_icon = document.querySelector(".menu-icon");
+
+// for the underline
 list_item.forEach(item => {
   item.addEventListener("click", function () {
     list_item.forEach(item => item.classList.remove("underlined"));
     item.classList.add("underlined");
   });
+});
+
+// for the sideBar
+menu_icon.addEventListener("click", function () {
+  others.classList.toggle("active");
+  console.log("fucking");
 });
